a09d9cc6dfb277908b2b08209bed4eff356ca867
franredhat <40667034+franredhat@users.noreply.github.com>
2018-07-02 17:52:47 +0200

    Nexus and probes

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -7,25 +7,24 @@ if [ "$#" -ne 1 ]; then
 fi
 
 GUID=$1
-echo "Setting up Nexus in project $GUID-nexus"
 
-# Code to set up the Nexus. It will need to
-# * Create Nexus
-# * Set the right options for the Nexus Deployment Config
-# * Load Nexus with the right repos
-# * Configure Nexus as a docker registry
-# Hint: Make sure to wait until Nexus if fully up and running
-#       before configuring nexus with repositories.
-#       You could use the following code:
-# while : ; do
-#   echo "Checking if Nexus is Ready..."
-#   oc get pod -n ${GUID}-nexus|grep '\-2\-'|grep -v deploy|grep "1/1"
-#   [[ "$?" == "1" ]] || break
-#   echo "...no. Sleeping 10 seconds."
-#   sleep 10
-# done
+echo "Setting Up Nexus in Project $GUID-nexus"
+oc project $GUID-nexus
+oc new-app -f https://raw.githubusercontent.com/franredhat/development-homework/master/Infrastructure/templates/nexus3-persistent-template.yaml
 
-# Ideally just calls a template
-# oc new-app -f ../templates/nexus.yaml --param .....
+echo "Waiting 10 minutes until Nexus can be configured"
+sleep 600
 
-# To be Implemented by Student
+echo "Setting up Nexus readiness and liveness probes"
+oc set probe dc/nexus3 --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok
+oc set probe dc/nexus3 --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/repository/maven-public/
+
+echo "Configuring Nexus with repositories"
+curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
+chmod +x setup_nexus3.sh
+./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
+rm setup_nexus3.sh
+
+echo "Adding Nexus Registry Route"
+oc expose dc nexus3 --port=5000 --name=nexus-registry
+oc create route edge nexus-registry --service=nexus-registry --port=5000
