1f74c47eddd91750af69ec5dea10f72845bc314a
franredhat <40667034+franredhat@users.noreply.github.com>
2018-07-02 17:54:49 +0200

    Sonar and probes

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -7,8 +7,12 @@ if [ "$#" -ne 1 ]; then
 fi
 
 GUID=$1
-echo "Setting up Sonarqube in project $GUID-sonarqube"
+echo "Setting up SonarQube in project $GUID-sonarqube"
+oc new-app -f https://raw.githubusercontent.com/franredhat/development-homework/master/Infrastructure/templates/sonarqube-postgresql-template.yaml -n $GUID-sonarqube
 
+echo "Setting up SonarQube liveness and readiness probes"
+oc set probe dc/sonarqube --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok
+oc set probe dc/sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url=http://:9000/about
 # Code to set up the SonarQube project.
 # Ideally just calls a template
 # oc new-app -f ../templates/sonarqube.yaml --param .....
