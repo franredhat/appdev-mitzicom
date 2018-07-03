a59830e54b4d032922e6ea652722939e30fc8e0c
franredhat <40667034+franredhat@users.noreply.github.com>
2018-07-03 12:30:34 +0200

    Added project namespace to each command

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -9,22 +9,21 @@ fi
 GUID=$1
 
 echo "Setting Up Nexus in Project $GUID-nexus"
-oc project $GUID-nexus
-oc new-app -f https://raw.githubusercontent.com/franredhat/development-homework/master/Infrastructure/templates/nexus3-persistent-template.yaml
+oc new-app -f https://raw.githubusercontent.com/franredhat/appdev-mitzicom/master/Infrastructure/templates/nexus3-persistent-template.yaml -n $GUID-nexus
 
 echo "Waiting 10 minutes until Nexus can be configured"
 sleep 600
 
 echo "Setting up Nexus readiness and liveness probes"
-oc set probe dc/nexus3 --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok
-oc set probe dc/nexus3 --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/repository/maven-public/
+oc set probe dc/nexus3 --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok -n $GUID-nexus
+oc set probe dc/nexus3 --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/repository/maven-public/ -n $GUID-nexus
 
 echo "Configuring Nexus with repositories"
 curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
 chmod +x setup_nexus3.sh
-./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
+./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}' -n $GUID-nexus)
 rm setup_nexus3.sh
 
 echo "Adding Nexus Registry Route"
-oc expose dc nexus3 --port=5000 --name=nexus-registry
-oc create route edge nexus-registry --service=nexus-registry --port=5000
+oc expose dc nexus3 --port=5000 --name=nexus-registry -n $GUID-nexus
+oc create route edge nexus-registry --service=nexus-registry --port=5000 -n $GUID-nexus
