8491c2383043a413cb6e2989423f79f304bc6e9a
franredhat <40667034+franredhat@users.noreply.github.com>
2018-07-03 12:31:48 +0200

    Solved some mistakes

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -8,11 +8,11 @@ fi
 
 GUID=$1
 echo "Setting up SonarQube in project $GUID-sonarqube"
-oc new-app -f https://raw.githubusercontent.com/franredhat/development-homework/master/Infrastructure/templates/sonarqube-postgresql-template.yaml -n $GUID-sonarqube
+oc new-app -f https://raw.githubusercontent.com/franredhat/appdev-mitzicom/master/Infrastructure/templates/sonarqube-postgresql-template.yaml -n $GUID-sonarqube
 
 echo "Setting up SonarQube liveness and readiness probes"
-oc set probe dc/sonarqube --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok
-oc set probe dc/sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url=http://:9000/about
+oc set probe dc/sonarqube --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok -n $GUID-sonarqube
+oc set probe dc/sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url=http://:9000/about -n $GUID-sonarqube
 # Code to set up the SonarQube project.
 # Ideally just calls a template
 # oc new-app -f ../templates/sonarqube.yaml --param .....
