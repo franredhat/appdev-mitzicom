3be4a9c45b0f0f75e76502717c4cc500fbadcdec
franredhat <40667034+franredhat@users.noreply.github.com>
2018-07-03 16:17:03 +0200

    removed probes permanently

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -9,7 +9,3 @@ fi
 GUID=$1
 echo "Setting up SonarQube in project $GUID-sonarqube"
 oc new-app -f https://raw.githubusercontent.com/franredhat/appdev-mitzicom/master/Infrastructure/templates/sonarqube-postgresql-template.yaml -n $GUID-sonarqube
-
-#echo "Setting up SonarQube liveness and readiness probes"
-#oc set probe dc/sonarqube --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok -n $GUID-sonarqube
-#c set probe dc/sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url=http://:9000/about -n $GUID-sonarqube
