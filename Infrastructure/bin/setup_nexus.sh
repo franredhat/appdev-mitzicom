4083d3fe5e023ec0420c159630802d2df9e0ae44
franredhat <40667034+franredhat@users.noreply.github.com>
2018-07-04 10:30:12 +0200

    removed nexus configuration

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -10,16 +10,3 @@ GUID=$1
 
 echo "Setting Up Nexus in Project $GUID-nexus"
 oc new-app -f https://raw.githubusercontent.com/franredhat/appdev-mitzicom/master/Infrastructure/templates/nexus3-persistent-template.yaml -n $GUID-nexus
-
-echo "Waiting until Nexus can be configured"
-sleep 500
-
-echo "Configuring Nexus with Wolfgang repositories"
-curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
-chmod +x setup_nexus3.sh
-./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}' -n $GUID-nexus)
-rm setup_nexus3.sh
-
-echo "Adding Nexus Registry Route"
-oc expose dc nexus3 --port=5000 --name=nexus-registry -n $GUID-nexus
-oc create route edge nexus-registry --service=nexus-registry --port=5000 -n $GUID-nexus
