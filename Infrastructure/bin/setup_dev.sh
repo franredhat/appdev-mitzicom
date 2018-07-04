2561b26b779c9a2cb45706d93405422f62f57e3f
franredhat <40667034+franredhat@users.noreply.github.com>
2018-07-04 11:12:53 +0200

    buildconfig pipelines

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -13,3 +13,9 @@ echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
 
 echo "Creating MongoDB single instance"
 oc new-app -f https://raw.githubusercontent.com/franredhat/appdev-mitzicom/master/Infrastructure/templates/mongodb-statefulset-1replicaset.yaml -n ${GUID}-parks-dev
+
+# Jenkins File Build config
+
+oc create -f https://raw.githubusercontent.com/franredhat/appdev-mitzicom/master/Infrastructure/templates/nationalparks-pipeline-buildconfig.yaml -n ${GUID}-jenkins
+oc create -f https://raw.githubusercontent.com/franredhat/appdev-mitzicom/master/Infrastructure/templates/mlbparks-pipeline-buildconfig.yaml -n ${GUID}-jenkins
+oc create -f https://raw.githubusercontent.com/franredhat/appdev-mitzicom/master/Infrastructure/templates/parksmap-pipeline-buildconfig.yaml -n ${GUID}-jenkins
