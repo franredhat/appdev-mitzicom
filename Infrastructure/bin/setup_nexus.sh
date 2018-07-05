0199e8591f397a7007646fefa7f854a6c37c7511
franredhat <40667034+franredhat@users.noreply.github.com>
2018-07-05 11:25:01 +0200

    added 5000 por service and route

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -10,3 +10,7 @@ GUID=$1
 
 echo "Setting Up Nexus in Project $GUID-nexus"
 oc new-app -f https://raw.githubusercontent.com/franredhat/appdev-mitzicom/master/Infrastructure/templates/nexus3-persistent-template.yaml -n $GUID-nexus
+
+echo "Adding 5000 Port Service and Route"
+oc expose dc nexus --port=5000 --name=nexus-registry -n $GUID-nexus
+oc create route edge nexus-registry --service=nexus-registry --port=5000 -n $GUID-nexus
