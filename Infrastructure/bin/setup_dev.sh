4086ef2267a05641a0ee0438ae4d369a05d2705b
franredhat <40667034+franredhat@users.noreply.github.com>
2018-07-06 18:59:04 +0200

    fixed error tasks instead of parks

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -18,8 +18,8 @@ echo "Creating MLBParks base app"
 oc new-build --binary=true --name="mlbparks" jboss-eap70-openshift:1.7 -n ${GUID}-parks-dev
 oc new-app ${GUID}-parks-dev/mlbparks:0.0-0 --name=mlbparks --allow-missing-imagestream-tags=true -n ${GUID}-parks-dev
 oc set triggers dc/mlbparks --remove-all -n ${GUID}-parks-dev
-oc expose dc mlbparks --port 8080 -n ${GUID}-tasks-dev
-oc expose svc mlbparks -n ${GUID}-tasks-dev
+oc expose dc mlbparks --port 8080 -n ${GUID}-parks-dev
+oc expose svc mlbparks -n ${GUID}-parks-dev
 
 echo "Creating Nationalparks base app"
 oc new-build --binary=true --name="nationalparks" redhat-openjdk18-openshift:1.2 -n ${GUID}-parks-dev
